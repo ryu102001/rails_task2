acc6be9f081c7cdb91846c37c6a9d89885faabf7
ushio ryuto <ryu102001@gmail.com>
2020-11-18 12:33:07 +0900

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,15 @@
 class UsersController < ApplicationController
 
-  #投稿したデータをデータベースから取り出すアクション
   def index
     @users = User.all
   end
 
-  #新規画面に対応するアクション
   def new
     @user = User.new
   end
 
-  #新規登録するためのアクション
   def create
-    User.create(user_params)#入力に制限をつけるメソッド
-    redirect_to action: :index
+    User.create(user_params)
   end
 
   def edit
@@ -23,17 +19,16 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
-    redirect_to action: :index
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy
-    redirect_to action: :index
+
   end
 
   private
-  def user_params#上記のメソッドを定義する
+  def user_params
     params.require(:user).permit(:name, :age)
   end
 end
